143f83264cd840223bf0bd8a53cee0dd7f86b0a1
cheesekaek <karthika.anusurek@gmail.com>
2026-03-27 07:12:23 -0400

    feat: update fish wing scraper with utils, dynamic headers, and cleaner location parsing

--- fish_wing_scraper.py
+++ fish_wing_scraper.py
@@ -1,7 +1,9 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-from expand import expand_table
+from utils import expand_table, get_header_indices
 
 def scrape_fw():
     # fish wing url scraper
@@ -34,18 +36,41 @@ def scrape_fw():
             if set_table:
                 grid = expand_table(set_table) # 2d grid
 
+                indices = get_header_indices(set_table) # indices for cols
+                name_index = indices.get("name")
+                image_index = indices.get("image")
+                location_index = indices.get("location")
+                weather_index = indices.get("weather") # weather index check required
+                rarity_index = indices.get("rarity")
+                size_index = indices.get("size")
+
                 rows = [] # list of rows in each set
                 for row in grid[1:]: # cols in each row : name, img_url, location, weather, rarity, size, completed
 
-                    link_tag = row[0].find("a")
-                    name = link_tag.get("title")
+                    link_tag = row[image_index].find("a")
                     img_tag = link_tag.find("img")
                     img_url = "https://fieldsofmistria.wiki.gg" + img_tag.get("src") # to account for lazy loading
 
-                    location = row[2].get_text(strip=True)
-                    weather = row[3].get_text(strip=True)
-                    rarity = row[4].get_text(strip=True)
-                    size = row[5].get_text(strip=True)
+                    name = row[name_index].get_text(strip=True)
+                    location = row[location_index].get_text(separator=" ", strip=True)
+                    location = re.sub(r"\s+\)", ")", location) # cleanup text "s+)" into ")"
+
+                    if weather_index is not None:
+                        a_tags = row[weather_index].find_all("a")
+                        weather = set()
+                        for a in a_tags:
+                            text = a.get_text(strip=True)
+                            if text:
+                                weather.add(text)
+                    else:
+                        weather = "All"
+
+                    if rarity_index is not None:
+                        rarity = row[rarity_index].get_text(strip=True)
+                    else:
+                        rarity = set_name
+
+                    size = row[size_index].get_text(strip=True)
 
                     rows.append({
                         "name": name,
