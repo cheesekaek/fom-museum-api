d284f301c62d6662a2e1f89edf14b22d413e0363
cheesekaek <karthika.anusurek@gmail.com>
2026-03-27 10:21:28 -0400

    feat: implement get_header_indices and location_parsing; modify locations to handle inconsistent HTML formatting

--- scrapers/archaeology_wing_scraper.py
+++ scrapers/archaeology_wing_scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from utils import expand_table
+from scrapers.utils import expand_table, parse_locations, get_header_indices
 
 def scrape_aw():
     url = "https://fieldsofmistria.wiki.gg/wiki/Archaeology_Wing"
@@ -30,21 +30,39 @@ def scrape_aw():
             if set_table:
                 grid = expand_table(set_table) # 2d grid
 
+                indices = get_header_indices(set_table) # indices for cols
+                name_index = indices.get("name")
+                image_index = indices.get("image")
+                location_index = indices.get("location")
+                rarity_index = indices.get("rarity")
+
                 rows = [] # list of rows in each set
                 for row in grid[1:]: # cols in each row : name, img_url, location, rarity, completed
 
-                    link_tag = row[0].find("a")
+                    link_tag = row[image_index].find("a")
                     img_tag = link_tag.find("img")
                     img_url = "https://fieldsofmistria.wiki.gg" + img_tag.get("src") # to account for lazy loading
 
-                    name = row[1].get_text(strip=True)
-                    location = row[2].get_text(separator=" ", strip=True)
-                    rarity = row[3].get_text(strip=True)
+                    name = row[name_index].get_text(strip=True)
+
+                    locations = parse_locations(row[location_index])
+                    final_loc = [] # custom modification for inconsistent HTML formats
+                    text = ""
+                    for loc in locations:
+                        if loc.startswith("(floors"):
+                            pass  # skip floor ranges entirely
+                        elif loc == "(requires Former Farmers Skill)":
+                            text += f" {loc}"
+                        else:
+                            final_loc.append(loc)
+                    final_loc = [loc + text if loc == "The Farm" else loc for loc in final_loc]
+
+                    rarity = row[rarity_index].get_text(strip=True)
 
                     rows.append({
                         "name": name,
                         "img_url": img_url,
-                        "location": location,
+                        "location(s)": final_loc,
                         "rarity": rarity,
                         "completed": False # default
                     })
