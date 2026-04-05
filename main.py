9af5113f61f3d182624857915ec6f44878b66c87
cheesekaek <karthika.anusurek@gmail.com>
2026-04-05 08:08:16 -0400

    feat: add api routes to get wings, items and/or sets + update "completed" for a specific item or items in a set

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from typing import Annotated
 from sqlmodel import Session, select
 from starlette.responses import JSONResponse
 
+from app.base_models import WingModel, ItemUpdate, ItemModel, IncludeSetItems, ItemSetModel
 from app.database import get_session
-from app.models import Wing
+from app.models import Wing, Item, ItemSet
 from scrapers.fish_wing_scraper import scrape_fw
 from scrapers.archaeology_wing_scraper import scrape_aw
 from scrapers.flora_wing_scraper import scrape_flw
@@ -22,12 +23,14 @@ app = FastAPI()
 # ---------------------------------------------------
 
 
-@app.get("/wings")
+# -------------------- GET /wings --------------------
+@app.get("/wings", response_model=list[WingModel], description="Get a list of all wings, including sets or items, or both")
 def get_wings(session: Session = Depends(get_session),
-              offset: int = 0,
-              limit: Annotated[int, Query(le=4)] = 4):
+              # toggle to include sets or items, or both
+              include: IncludeSetItems = Depends(),
+              limit: Annotated[int, Query(ge=1, le=4)] = 4):
 
-    wings = session.exec(select(Wing).offset(offset).limit(limit)).all()
+    wings = session.exec(select(Wing).limit(limit)).all()
 
     result = []
 
@@ -36,42 +39,76 @@ def get_wings(session: Session = Depends(get_session),
         wing_data = {
             "id": wing.id,
             "name": wing.name,
-            "sets": []
         }
-        # set attributes
-        for s in wing.sets:
-            set_data = {
-                "id": s.id,
-                "name": s.name,
-                "items": []
-            }
-            # item attributes
-            for item in s.items:
-                item_data = {
-                    # common attr
-                    "id": item.id,
-                    "name": item.name,
-                    "img": item.img,
-                    "completed": item.completed,
-                    # varied attr
-                    "locations": item.locations,
-                    "rarity": item.rarity,
-                    "weather": item.weather,
-                    "size": item.size,
-                    "sources": item.sources,
-                    "seasons": item.seasons,
-                    "time": item.time
+
+        # if sets is set to True
+        if include.sets:
+            wing_data["sets"] = []
+            # set attributes
+            for s in wing.sets:
+                set_data = {
+                    "id": s.id,
+                    "name": s.name,
                 }
-                # add each item to set
-                set_data["items"].append(item_data)
-            # add each set to a wing
-            wing_data["sets"].append(set_data)
+                # if items is set to True
+                if include.items:
+                    # add each item to set
+                    set_data["items"] = [ItemModel.model_validate(item) for item in s.items]
+                # add each set to a wing
+                wing_data["sets"].append(set_data)
+
+        # if items is set to True
+        elif include.items:
+            wing_data["items"] = [
+                ItemModel.model_validate(item)
+                for s in wing.sets
+                for item in s.items
+            ]
+
         # add each wing to result
         result.append(wing_data)
+
     # return list
     return result
 
 
+# ------------------ PATCH /items/{items_id} --------------------
+@app.patch("/items/{item_id}", response_model=ItemModel, description="Mark an item as completed")
+def update_item(update: ItemUpdate,
+                item_id: int = 1,
+                session: Session = Depends(get_session)):
+    # find item
+    item = session.get(Item, item_id)
+
+    if not item:
+        raise HTTPException(status_code=404, detail="This item cannot be found.")
+
+    # update item
+    item.completed = update.completed
+    session.commit()
+    session.refresh(item)
+
+    return ItemModel.model_validate(item)
+
+# ------------------ PATCH /sets/{sets_id} --------------------
+@app.patch("/sets/{sets_id}", response_model=ItemSetModel, description="Mark a set as completed")
+def update_set(update: ItemUpdate,
+               sets_id: int = 1,
+               session: Session = Depends(get_session)):
+    # find set
+    item_set = session.get(ItemSet, sets_id)
+    
+    if not item_set:
+        raise HTTPException(status_code=404, detail="This set cannot be found.")
+
+    # find each item in set
+    for item in item_set.items:
+        # update item
+        item.completed = update.completed
+
+    session.commit()
+    session.refresh(item_set)
+    return ItemSetModel.model_validate(item_set)
 
 
 # ----------------------------------------------------
